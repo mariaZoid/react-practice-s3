511f47ea08763152b2425738f97553c97792758d
Maria P <maria@zoidpay.com>
2023-09-20 10:45:42 +0300

    add if else statement

--- changePrice.sh
+++ changePrice.sh
@@ -1,4 +1,10 @@
 # declare api_url = "https://api.coingecko.com/api/v3/coins/zoid-pay"
-price=$(curl https://api.coingecko.com/api/v3/coins/zoid-pay | jq -r '.market_data.current_price.usd')
+export ENVIRONMENT="dev"
 
-sed -i -e "s/10/$price/g" src/App.js
+if [ "$ENVIRONMENT" = "dev" ]; then
+    price=$(curl https://api.coingecko.com/api/v3/coins/zoid-pay | jq -r '.market_data.current_price.usd')
+
+    sed -i -e "s/10/$price/g" src/App.js
+else
+    echo "The environment is not 'dev'."
+fi
